1a1bc9e15bfc94665e224b2e4947f0038a267475
Jakov Sosic <jsosic@gmail.com>
2016-09-30 03:25:06 +0200

    Add dependency to Java module

--- manifests/dependency.pp
+++ manifests/dependency.pp
@@ -4,6 +4,6 @@
 # This module contains dependencies for Tomcat
 #
 class tomcat::dependency {
-  include ::java::oracle7
+  include ::java::openjdk8
 }
 # vi:syntax=puppet:filetype=puppet:ts=4:et:nowrap:
